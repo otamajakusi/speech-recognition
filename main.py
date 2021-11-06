8d724ab757a64e98cc8c330a07a6f0677c95cae0
hiroyuki.obinata <hiroyuki.obinata@gmail.com>
2021-11-06 11:58:19 +0900

    add apikey argument

--- main.py
+++ main.py
@@ -3,18 +3,17 @@ import speech_recognition as sr
 TIMEOUT = 3
 
 
-def speech2text(lang):
+def speech2text(lang, apikey):
     r = sr.Recognizer()
     with sr.Microphone() as src:
         r.adjust_for_ambient_noise(src)
 
         print(f"speak up for {TIMEOUT} seconds")
         audio = r.listen(src, timeout=TIMEOUT)
-        print("end")
+        print("finished")
 
     try:
-        # text = r.recognize_google(audio, language="ja-JP")
-        text = r.recognize_google(audio, language=lang)
+        text = r.recognize_google(audio, language=lang, key=apikey)
         print(f"you said: {text}")
     except Exception as e:
         print(e)
@@ -29,9 +28,16 @@ def main():
         default="en-US",
         help="select language. default 'en-US'. 'ja-JP' can be used.",
     )
+    parser.add_argument(
+        "--apikey",
+        default=None,
+        help="""Google Speech API key. If not specified default key is used.
+        To obtain you own API key, follow the instruction below:
+        http://www.chromium.org/developers/how-tos/api-keys""",
+    )
     args = parser.parse_args()
 
-    speech2text(args.lang)
+    speech2text(args.lang, args.apikey)
 
 
 main()
